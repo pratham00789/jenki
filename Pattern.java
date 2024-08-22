3bf5599a0834d6e6f4adbbc9cebb581f27e693c1
pratham00789 <119481020+pratham00789@users.noreply.github.com>
2024-08-22 20:56:10 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,5 +1,6 @@
 public class Pattern{
     public static void main(String[] args) {
         System.out.println("Hello, World! to the jenkins in the start");
+        System.out.println("hello again")
     }
 }
